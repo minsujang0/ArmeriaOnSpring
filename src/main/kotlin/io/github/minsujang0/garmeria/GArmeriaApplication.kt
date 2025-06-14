49be750cf1334488f5918960ee00c56e18849200
Minsu Jang <minsujangk@gmail.com>
2025-06-14 15:48:31 +0900

    feat(project-setup): add initial project setup with Spring Boot and Gradle

---
+++ src/main/kotlin/io/github/minsujang0/garmeria/GArmeriaApplication.kt
@@ -0,0 +1,11 @@
+package io.github.minsujang0.garmeria
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GArmeriaApplication
+
+fun main(args: Array<String>) {
+    runApplication<GArmeriaApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/minsujang0/garmeria/GArmeriaApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.minsujang0.garmeria
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GArmeriaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
